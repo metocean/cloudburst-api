1a5c933ee3ec692205c5808a8b09af12f9ce2a3a
alpha-beta-soup <r.law@metocean.co.nz>
2016-06-07 18:49:45 +1200

    rename

--- src/cloudburst.leaflet.tilelayer.coffee
+++ src/cloudburst.leaflet.tilelayer.coffee
@@ -1,5 +1,3 @@
-logging = on
-
 L.CloudburstTileLayer = L.TileLayer.extend
   options:
     minZoom: 0
@@ -8,214 +6,95 @@ L.CloudburstTileLayer = L.TileLayer.extend
     zoomOffset: 0
     detectRetina: true
 
-  initialize: (cloudbursturl, config, options) ->
-    # cloudbursturl: host for capability requests
-    if not config
-      return
-    @_host = cloudbursturl
-    @_config = config
-    @_layers = @getLayers()
+  initialize: (urlTemplate, times, levels, bounds, options) ->
+    @_times = if times? then times else null
+    @_levels = if levels? then levels else null
 
-    # Select the first available layer, and first instance, index
-    layer_i = 0
-    while !@_instance and layer_i < @_layers.length
-      @setLayer(@_layers[layer_i], no) # Initially set to first layer
-      @setInstance(@getInstances()[0], no) # Initially set to first instance of layer
-      if !@_instance
-        layer_i = layer_i + 1
-        console.log("WARNING: no instances of layer #{@_layer} found")
+    @hasTimes = if @_times then true else false
+    @hasLevels = if @_levels then true else false
 
-    @setTindex(@getTindexes()[0], no)
-    @setLevel(@getLevels()[0], no)
-    @setRenderer('mpl', no)
+    @setTime(if @_times? then @_times[0] else 0)
+    @setLevel(if @_levels? then @_levels[0] else 0)
 
-    L.TileLayer.prototype.initialize.call @, "[cloudburst]/{z}/{x}/{y}.png", options
+    @bounds = if bounds? then bounds else null
 
-  getConfig: ->
-    @_config
-
-  getLayers: (asObj, sorted) ->
-    # asObj (bool):
-    # - true: returns the layers as an array of tuples: (short name, json)
-    # - false: returns the layers as an array of short names
-    sorted = if sorted? then sorted else true
-    if @_config?
-      if !asObj? or !asObj
-        lyrs = Object.keys(@_config)
-      else
-        lyrs = ([lyr, @_config[lyr]] for lyr in Object.keys(@_config))
-      # console.log lyrs[0][1].meta.name
-      if sorted
-        lyrs.sort()
-      return lyrs
-
-  setLayer: (layer, noRedraw) ->
-    if (layer in Object.keys(@_config))
-      @_layer = layer
-      @redraw() if !noRedraw? or !noRedraw
-      if logging is on
-        console.log("Layer set to: #{@_layer}")
-    @
-
-  getLayer: ->
-    @_layer
-
-  getLayerLegendUrl: (size, orientation) ->
-    if !size? or !size
-      size = "small" # or "large"
-    if !orientation? or !orientation
-      orientation = "horiztonal" # or "vertical"
-    layerurl = "#{@_host}/legend/#{size}/#{orientation}/#{@getLayer()}/#{@getInstance()}.png"
-    return layerurl
+    L.TileLayer.prototype.initialize.call(@, urlTemplate, options, bounds, times, levels)
 
   getBounds: ->
     # Returns bounds as L.latLngBounds
-    # TODO enforce this for layers by default to avoid requesting blank tiles
-    if @_layer? and @_instance?
-      bounds = @_config[@_layer]['dataset'][@_instance]['bounds']
+    if @bounds?
       return new L.latLngBounds(
-        L.latLng(bounds['south'], bounds['west']),
-        L.latLng(bounds['north'], bounds['east'])
+        L.latLng(@bounds['south'], @bounds['west']),
+        L.latLng(@bounds['north'], @bounds['east'])
       )
 
-  getLayerMetadata: ->
-    if @_layer? and @_instance?
-      @_config[@_layer].meta
-
-  getLayerDescription: ->
-    if @_layer?
-      @getLayerMetadata().description
-
-  getLayerName: ->
-    if @_layer?
-      @getLayerMetadata().name
-
-  getLayerUnitSystem: ->
-    if @_layer?
-      @getLayerMetadata().unit_system
-
-  getLayerPlotDefinitions: ->
-    if @_layer
-      @_config.layers[@_layer].plot_defs
-
-  getInstances: (asObj, sorted)->
-    sorted = if sorted? then sorted else true
-    if @_layer?
-      instances = Object.keys(@_config[@_layer]['dataset'])
-      if asObj? and asObj
-        instances = ([i, @_config[@_layer]['dataset'][i]] for i in instances)
-      if sorted
-        instances.sort().reverse() # Newest first
-      return instances
-
-  setInstance: (instance, noRedraw) ->
-    if !instance?
-      console.log("WARNING: instance undefined")
-    if instance in @getInstances()
-      @_instance = instance
-      @redraw() if !noRedraw? or !noRedraw
-      if logging is on
-        console.log("Instance set to: #{@_instance}")
-    @
-
-  getInstance: ->
-    @_instance
-
-  getLevels: (asObj) ->
+  getLevels: ->
     # Vertical dimension
-    if @_instance? and @_layer? and 'levels' in Object.keys @_config[@_layer]['dataset'][@_instance]
-      levels = Object.keys(@_config[@_layer]['dataset'][@_instance]['levels'])
-      if asObj? and asObj
-        levels = ([i, @_config[@_layer]['dataset'][@_instance]['levels'][i]] for i in levels)
-      return levels
-    return if !asObj then ["0"] else {"0": undefined}
+    @_levels
 
   setLevel: (level, noRedraw) ->
+    if !@hasLevels
+      return @
     if level.toString() in @getLevels()
       @_level = level.toString()
       @redraw() if !noRedraw? or !noRedraw
-      if logging is on
-        console.log("Level set to: #{@_level}")
     @
 
   getLevel: ->
     @_level
 
-  getTindexes: (asObj) ->
-    # Time-indexes (tindexes)
-    if @_instance? and @_layer? and 'times' in Object.keys @_config[@_layer]['dataset'][@_instance]
-      tindexes = Object.keys(@_config[@_layer]['dataset'][@_instance].times)
-      if asObj? and asObj
-        tindexes = ([i, @_config[@_layer]['dataset'][@_instance].times[i]] for i in tindexes)
-      return tindexes
-    return if !asObj then ["0"] else [["0", undefined]]
+  getTimes: ->
+    @_times
 
-  setTindex: (tindex, noRedraw) ->
-    if tindex.toString() in @getTindexes()
-      @_tindex = tindex.toString()
+  setTime: (time, noRedraw) ->
+    if !@hasTimes
+      @_time = 0
+      return @
+    else if time in @getTimes()
+      @_time = time.toString()
       @redraw() if !noRedraw? or !noRedraw
-      if logging is on
-        console.log("Tindex set to: #{@_tindex}")
     @
 
-  getTindex: (as_time_string) ->
-    if !as_time_string?
-      return @_tindex
-    else
-      return @getTindexes(yes)[@_tindex][1]
-
-  getRenderer: ->
-    @_renderer
-
-  setRenderer: (renderer, noRedraw) ->
-    @_renderer = renderer
-    @redraw() if !noRedraw? or !noRedraw
-    @
+  getTime: ->
+    @_time
 
   back: (noRedraw) ->
-    if @_tindex?
-      if parseInt(@_tindex) > 0
-        @setTindex(Math.max(parseInt(@_tindex) - 1, 0), noRedraw)
+    if !@hasTimes
+      return
+    t = @getTime()
+    times = @getTimes()
+    if t? and times.indexOf(t) > 0
+      @setTime(times[times.indexOf(t)-1], noRedraw)
 
   forward: (noRedraw) ->
-    if @_tindex?
-      if parseInt(@_tindex) < @getTindexes().length - 1
-        @setTindex(Math.min(parseInt(@_tindex) + 1, @getTindexes().length - 1), noRedraw)
+    if !@hasTimes
+      return
+    t = @getTime()
+    times = @getTimes()
+    if t? and times.indexOf(t) < times.length - 1
+      @setTime(times[times.indexOf(t)+1], noRedraw)
 
   higher: (noRedraw) ->
-    if logging is on
-      console.log "Going higher: #{@_level}"
-    if parseInt(@_level) > 0
-      @setLevel(Math.max(parseInt(@_level) - 1, 0), noRedraw)
+    if !@hasLevels
+      return
+    l = @getLevel()
+    levels = @getLevels()
+    if l? and levels.indexOf(l) < levels.length - 1
+      @setLevel(levels[levels.indexOf(l)+1], noRedraw)
 
   deeper: (noRedraw) ->
-    if logging is on
-      console.log "Going deeper: #{@_level}"
-    if parseInt(@_level) < @getLevels().length - 1
-      @setLevel(Math.min(parseInt(@_level) + 1, @getLevels().length - 1), noRedraw)
+    if !@hasLevels
+      return
+    l = @getLevel()
+    levels = @getLevels()
+    if l? and levels.indexOf(l) > 0
+      @setLevel(levels[levels.indexOf(l)-1], noRedraw)
 
   getTileUrl: (coords) ->
-    # Replace [cloudburst] in the prototype URL with parameters from the config
     L.TileLayer.prototype.getTileUrl
-      .call @, coords
-      .replace /\[cloudburst\]/,
-        "#{@_host}/tile/#{@_renderer}/#{@_layer}/#{@_instance}/#{@_tindex}/#{@_level}"
+      .call(@, coords)
+      .replace('<time>', if !@hasTimes then 0 else @getTimes().indexOf(@getTime()))
+      .replace('<level>', if !@hasLevels then 0 else @getLevels().indexOf(@getLevel()))
 
-  getTindexesAsPercetagePositions: ->
-    # Takes an array of datetime strings from @getTindexes, and returns an array
-    # of the same length where the datetimes are represented as floats
-    # indicating their percentage value from 0 to 100, where 0 is the lowest
-    # value in the array, and 100 is the highest value.
-    # Useful for displaying an array of time values in irregular intervals along
-    # a line.
-    ts = (moment(t[1], moment.ISO_8601)/1000 for t in @getTindexes())
-    min_ts = Math.min.apply(null, ts)
-    max_ts = Math.max.apply(null, ts)
-    diff = max_ts - min_ts
-    return (t / diff * 100 for t in (t - min_ts for t in ts))
-
-L.cloudburstTileLayer = (cloudbursturl, json, options) ->
-  new L.CloudburstTileLayer cloudbursturl, json, options
-
-# L.cloudburstInstance = (cloudbursturl, layerid, instanceid, options) ->
-#   new L.CloudburstInstance cloudbursturl, layerid, instanceid
+L.cloudburstTileLayer = (urlTemplate, times, levels, bounds, options) ->
+  new L.CloudburstTileLayer(urlTemplate, times, levels, bounds, options)
--- src/cloudburst.leaflet.tilelayer.js
+++ src/cloudburst.leaflet.tilelayer.js
@@ -1,7 +1,4 @@
-var logging,
-  indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
-
-logging = true;
+var indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
 L.CloudburstTileLayer = L.TileLayer.extend({
   options: {
@@ -11,187 +8,33 @@ L.CloudburstTileLayer = L.TileLayer.extend({
     zoomOffset: 0,
     detectRetina: true
   },
-  initialize: function(cloudbursturl, config, options) {
-    var layer_i;
-    if (!config) {
-      return;
-    }
-    this._host = cloudbursturl;
-    this._config = config;
-    this._layers = this.getLayers();
-    layer_i = 0;
-    while (!this._instance && layer_i < this._layers.length) {
-      this.setLayer(this._layers[layer_i], false);
-      this.setInstance(this.getInstances()[0], false);
-      if (!this._instance) {
-        layer_i = layer_i + 1;
-        console.log("WARNING: no instances of layer " + this._layer + " found");
-      }
-    }
-    this.setTindex(this.getTindexes()[0], false);
-    this.setLevel(this.getLevels()[0], false);
-    this.setRenderer('mpl', false);
-    return L.TileLayer.prototype.initialize.call(this, "[cloudburst]/{z}/{x}/{y}.png", options);
-  },
-  getConfig: function() {
-    return this._config;
-  },
-  getLayers: function(asObj, sorted) {
-    var lyr, lyrs;
-    sorted = sorted != null ? sorted : true;
-    if (this._config != null) {
-      if ((asObj == null) || !asObj) {
-        lyrs = Object.keys(this._config);
-      } else {
-        lyrs = (function() {
-          var j, len, ref, results;
-          ref = Object.keys(this._config);
-          results = [];
-          for (j = 0, len = ref.length; j < len; j++) {
-            lyr = ref[j];
-            results.push([lyr, this._config[lyr]]);
-          }
-          return results;
-        }).call(this);
-      }
-      if (sorted) {
-        lyrs.sort();
-      }
-      return lyrs;
-    }
-  },
-  setLayer: function(layer, noRedraw) {
-    if ((indexOf.call(Object.keys(this._config), layer) >= 0)) {
-      this._layer = layer;
-      if ((noRedraw == null) || !noRedraw) {
-        this.redraw();
-      }
-      if (logging === true) {
-        console.log("Layer set to: " + this._layer);
-      }
-    }
-    return this;
-  },
-  getLayer: function() {
-    return this._layer;
-  },
-  getLayerLegendUrl: function(size, orientation) {
-    var layerurl;
-    if ((size == null) || !size) {
-      size = "small";
-    }
-    if ((orientation == null) || !orientation) {
-      orientation = "horiztonal";
-    }
-    layerurl = this._host + "/legend/" + size + "/" + orientation + "/" + (this.getLayer()) + "/" + (this.getInstance()) + ".png";
-    return layerurl;
+  initialize: function(urlTemplate, times, levels, bounds, options) {
+    this._times = times != null ? times : null;
+    this._levels = levels != null ? levels : null;
+    this.hasTimes = this._times ? true : false;
+    this.hasLevels = this._levels ? true : false;
+    this.setTime(this._times != null ? this._times[0] : 0);
+    this.setLevel(this._levels != null ? this._levels[0] : 0);
+    this.bounds = bounds != null ? bounds : null;
+    return L.TileLayer.prototype.initialize.call(this, urlTemplate, options, bounds, times, levels);
   },
   getBounds: function() {
-    var bounds;
-    if ((this._layer != null) && (this._instance != null)) {
-      bounds = this._config[this._layer]['dataset'][this._instance]['bounds'];
-      return new L.latLngBounds(L.latLng(bounds['south'], bounds['west']), L.latLng(bounds['north'], bounds['east']));
+    if (this.bounds != null) {
+      return new L.latLngBounds(L.latLng(this.bounds['south'], this.bounds['west']), L.latLng(this.bounds['north'], this.bounds['east']));
     }
   },
-  getLayerMetadata: function() {
-    if ((this._layer != null) && (this._instance != null)) {
-      return this._config[this._layer].meta;
-    }
-  },
-  getLayerDescription: function() {
-    if (this._layer != null) {
-      return this.getLayerMetadata().description;
-    }
-  },
-  getLayerName: function() {
-    if (this._layer != null) {
-      return this.getLayerMetadata().name;
-    }
-  },
-  getLayerUnitSystem: function() {
-    if (this._layer != null) {
-      return this.getLayerMetadata().unit_system;
-    }
-  },
-  getLayerPlotDefinitions: function() {
-    if (this._layer) {
-      return this._config.layers[this._layer].plot_defs;
-    }
-  },
-  getInstances: function(asObj, sorted) {
-    var i, instances;
-    sorted = sorted != null ? sorted : true;
-    if (this._layer != null) {
-      instances = Object.keys(this._config[this._layer]['dataset']);
-      if ((asObj != null) && asObj) {
-        instances = (function() {
-          var j, len, results;
-          results = [];
-          for (j = 0, len = instances.length; j < len; j++) {
-            i = instances[j];
-            results.push([i, this._config[this._layer]['dataset'][i]]);
-          }
-          return results;
-        }).call(this);
-      }
-      if (sorted) {
-        instances.sort().reverse();
-      }
-      return instances;
-    }
-  },
-  setInstance: function(instance, noRedraw) {
-    if (instance == null) {
-      console.log("WARNING: instance undefined");
-    }
-    if (indexOf.call(this.getInstances(), instance) >= 0) {
-      this._instance = instance;
-      if ((noRedraw == null) || !noRedraw) {
-        this.redraw();
-      }
-      if (logging === true) {
-        console.log("Instance set to: " + this._instance);
-      }
-    }
-    return this;
-  },
-  getInstance: function() {
-    return this._instance;
-  },
-  getLevels: function(asObj) {
-    var i, levels;
-    if ((this._instance != null) && (this._layer != null) && indexOf.call(Object.keys(this._config[this._layer]['dataset'][this._instance]), 'levels') >= 0) {
-      levels = Object.keys(this._config[this._layer]['dataset'][this._instance]['levels']);
-      if ((asObj != null) && asObj) {
-        levels = (function() {
-          var j, len, results;
-          results = [];
-          for (j = 0, len = levels.length; j < len; j++) {
-            i = levels[j];
-            results.push([i, this._config[this._layer]['dataset'][this._instance]['levels'][i]]);
-          }
-          return results;
-        }).call(this);
-      }
-      return levels;
-    }
-    if (!asObj) {
-      return ["0"];
-    } else {
-      return {
-        "0": void 0
-      };
-    }
+  getLevels: function() {
+    return this._levels;
   },
   setLevel: function(level, noRedraw) {
     var ref;
+    if (!this.hasLevels) {
+      return this;
+    }
     if (ref = level.toString(), indexOf.call(this.getLevels(), ref) >= 0) {
       this._level = level.toString();
       if ((noRedraw == null) || !noRedraw) {
         this.redraw();
-      }
-      if (logging === true) {
-        console.log("Level set to: " + this._level);
       }
     }
     return this;
@@ -199,128 +42,73 @@ L.CloudburstTileLayer = L.TileLayer.extend({
   getLevel: function() {
     return this._level;
   },
-  getTindexes: function(asObj) {
-    var i, tindexes;
-    if ((this._instance != null) && (this._layer != null) && indexOf.call(Object.keys(this._config[this._layer]['dataset'][this._instance]), 'times') >= 0) {
-      tindexes = Object.keys(this._config[this._layer]['dataset'][this._instance].times);
-      if ((asObj != null) && asObj) {
-        tindexes = (function() {
-          var j, len, results;
-          results = [];
-          for (j = 0, len = tindexes.length; j < len; j++) {
-            i = tindexes[j];
-            results.push([i, this._config[this._layer]['dataset'][this._instance].times[i]]);
-          }
-          return results;
-        }).call(this);
-      }
-      return tindexes;
-    }
-    if (!asObj) {
-      return ["0"];
-    } else {
-      return [["0", void 0]];
-    }
+  getTimes: function() {
+    return this._times;
   },
-  setTindex: function(tindex, noRedraw) {
-    var ref;
-    if (ref = tindex.toString(), indexOf.call(this.getTindexes(), ref) >= 0) {
-      this._tindex = tindex.toString();
+  setTime: function(time, noRedraw) {
+    if (!this.hasTimes) {
+      this._time = 0;
+      return this;
+    } else if (indexOf.call(this.getTimes(), time) >= 0) {
+      this._time = time.toString();
       if ((noRedraw == null) || !noRedraw) {
         this.redraw();
       }
-      if (logging === true) {
-        console.log("Tindex set to: " + this._tindex);
-      }
     }
     return this;
   },
-  getTindex: function(as_time_string) {
-    if (as_time_string == null) {
-      return this._tindex;
-    } else {
-      return this.getTindexes(true)[this._tindex][1];
-    }
-  },
-  getRenderer: function() {
-    return this._renderer;
-  },
-  setRenderer: function(renderer, noRedraw) {
-    this._renderer = renderer;
-    if ((noRedraw == null) || !noRedraw) {
-      this.redraw();
-    }
-    return this;
+  getTime: function() {
+    return this._time;
   },
   back: function(noRedraw) {
-    if (this._tindex != null) {
-      if (parseInt(this._tindex) > 0) {
-        return this.setTindex(Math.max(parseInt(this._tindex) - 1, 0), noRedraw);
-      }
+    var t, times;
+    if (!this.hasTimes) {
+      return;
+    }
+    t = this.getTime();
+    times = this.getTimes();
+    if ((t != null) && times.indexOf(t) > 0) {
+      return this.setTime(times[times.indexOf(t) - 1], noRedraw);
     }
   },
   forward: function(noRedraw) {
-    if (this._tindex != null) {
-      if (parseInt(this._tindex) < this.getTindexes().length - 1) {
-        return this.setTindex(Math.min(parseInt(this._tindex) + 1, this.getTindexes().length - 1), noRedraw);
-      }
+    var t, times;
+    if (!this.hasTimes) {
+      return;
+    }
+    t = this.getTime();
+    times = this.getTimes();
+    if ((t != null) && times.indexOf(t) < times.length - 1) {
+      return this.setTime(times[times.indexOf(t) + 1], noRedraw);
     }
   },
   higher: function(noRedraw) {
-    if (logging === true) {
-      console.log("Going higher: " + this._level);
+    var l, levels;
+    if (!this.hasLevels) {
+      return;
     }
-    if (parseInt(this._level) > 0) {
-      return this.setLevel(Math.max(parseInt(this._level) - 1, 0), noRedraw);
+    l = this.getLevel();
+    levels = this.getLevels();
+    if ((l != null) && levels.indexOf(l) < levels.length - 1) {
+      return this.setLevel(levels[levels.indexOf(l) + 1], noRedraw);
     }
   },
   deeper: function(noRedraw) {
-    if (logging === true) {
-      console.log("Going deeper: " + this._level);
+    var l, levels;
+    if (!this.hasLevels) {
+      return;
     }
-    if (parseInt(this._level) < this.getLevels().length - 1) {
-      return this.setLevel(Math.min(parseInt(this._level) + 1, this.getLevels().length - 1), noRedraw);
+    l = this.getLevel();
+    levels = this.getLevels();
+    if ((l != null) && levels.indexOf(l) > 0) {
+      return this.setLevel(levels[levels.indexOf(l) - 1], noRedraw);
     }
   },
   getTileUrl: function(coords) {
-    return L.TileLayer.prototype.getTileUrl.call(this, coords).replace(/\[cloudburst\]/, this._host + "/tile/" + this._renderer + "/" + this._layer + "/" + this._instance + "/" + this._tindex + "/" + this._level);
-  },
-  getTindexesAsPercetagePositions: function() {
-    var diff, max_ts, min_ts, t, ts;
-    ts = (function() {
-      var j, len, ref, results;
-      ref = this.getTindexes();
-      results = [];
-      for (j = 0, len = ref.length; j < len; j++) {
-        t = ref[j];
-        results.push(moment(t[1], moment.ISO_8601) / 1000);
-      }
-      return results;
-    }).call(this);
-    min_ts = Math.min.apply(null, ts);
-    max_ts = Math.max.apply(null, ts);
-    diff = max_ts - min_ts;
-    return (function() {
-      var j, len, ref, results;
-      ref = (function() {
-        var k, len, results1;
-        results1 = [];
-        for (k = 0, len = ts.length; k < len; k++) {
-          t = ts[k];
-          results1.push(t - min_ts);
-        }
-        return results1;
-      })();
-      results = [];
-      for (j = 0, len = ref.length; j < len; j++) {
-        t = ref[j];
-        results.push(t / diff * 100);
-      }
-      return results;
-    })();
+    return L.TileLayer.prototype.getTileUrl.call(this, coords).replace('<time>', !this.hasTimes ? 0 : this.getTimes().indexOf(this.getTime())).replace('<level>', !this.hasLevels ? 0 : this.getLevels().indexOf(this.getLevel()));
   }
 });
 
-L.cloudburstTileLayer = function(cloudbursturl, json, options) {
-  return new L.CloudburstTileLayer(cloudbursturl, json, options);
+L.cloudburstTileLayer = function(urlTemplate, times, levels, bounds, options) {
+  return new L.CloudburstTileLayer(urlTemplate, times, levels, bounds, options);
 };
